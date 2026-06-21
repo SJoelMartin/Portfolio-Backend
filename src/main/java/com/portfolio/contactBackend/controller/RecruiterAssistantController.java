fb87967967b84df30dbc792eb07adc32021ed6b1
Joel Martin <sjoelmartin.1703@gmail.com>
2026-06-21 16:57:21 +0530

    Added Logs for Recruiter Assistant

--- src/main/java/com/portfolio/contactBackend/controller/RecruiterAssistantController.java
+++ src/main/java/com/portfolio/contactBackend/controller/RecruiterAssistantController.java
@@ -1,8 +1,11 @@
 package com.portfolio.contactBackend.controller;
 
 import java.io.IOException;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,12 +23,19 @@ public class RecruiterAssistantController {
 
     @Autowired
     private GeminiService geminiService;
-
+    
     @PostMapping("/recruiter-assistant")
-    public ChatResponse ask(@RequestBody ChatRequest request) throws IOException {
-    	String answer =
-                geminiService.askGemini(request.getMessage());
+    public ResponseEntity<?> ask(@RequestBody ChatRequest request) {
+        try {
+            String response = geminiService.askGemini(request.getMessage());
+            return ResponseEntity.ok(new ChatResponse(response));
 
-        return new ChatResponse(answer);
+        } catch(Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of(
+                        "error", e.getMessage()
+                    ));
+        }
     }
 }
