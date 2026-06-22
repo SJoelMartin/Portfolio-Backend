f8ddf74350259a916eee113e441d0cc1e92574b7
Joel Martin <115999666+SJoelMartin@users.noreply.github.com>
2026-06-22 11:22:11 +0530

    Permit OPTIONS and specific API endpoints in SecurityConfig

--- src/main/java/com/portfolio/contactBackend/config/SecurityConfig.java
+++ src/main/java/com/portfolio/contactBackend/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig {
 	            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 	        )
 	        .authorizeHttpRequests(auth -> auth
+				.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 	            .requestMatchers("/api/auth/login").permitAll()
 	            .requestMatchers("/api/user/register").permitAll()
 	            .requestMatchers("/api/recruiter-assistant").permitAll()
